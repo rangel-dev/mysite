5e2aa7b0a06589c10a93ff41df8ffcb56f06bc68
Marcos Rangel Dias de Cerqueira <109158520+rangel-dev@users.noreply.github.com>
2024-09-08 17:27:37 -0300

    first commit

---
+++ precache-manifest.99b390d222e367b63ba1eecf5fd788f1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a83763bd5939cd591e4e7cf8c4dbd29a",
+    "url": "/mysite/index.html"
+  },
+  {
+    "revision": "4c1e59d0a1c30895de21",
+    "url": "/mysite/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "ca6f748aa941c955bb61",
+    "url": "/mysite/static/css/main.338fac54.chunk.css"
+  },
+  {
+    "revision": "4c1e59d0a1c30895de21",
+    "url": "/mysite/static/js/2.fa286f51.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/mysite/static/js/2.fa286f51.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ca6f748aa941c955bb61",
+    "url": "/mysite/static/js/main.be70d383.chunk.js"
+  },
+  {
+    "revision": "92473413ea62730f6359",
+    "url": "/mysite/static/js/runtime-main.43c8b920.js"
+  }
+]);
